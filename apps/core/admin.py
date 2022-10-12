19c4e364e5eea7618d97ab619b114bc4ed77114a
james <apodisation13@gmail.com>
2022-10-12 12:04:57 +0300

    app core - Color, Brand, Car models added + admin for models

--- apps/core/admin.py
+++ apps/core/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from apps.core.models import Brand, Car, Color
+
+admin.site.register(Color)
+admin.site.register(Brand)
+admin.site.register(Car)
--- apps/core/models.py
+++ apps/core/models.py
@@ -1,3 +1,43 @@
 from django.db import models
 
-# Create your models here.
+
+class Color(models.Model):
+    """Модель цвета машин"""
+    name = models.CharField(max_length=32,
+                            blank=False, null=False,
+                            unique=True,
+                            verbose_name="Название цвета")
+
+    def __str__(self):
+        return f'{self.id} - {self.name}'
+
+
+class Brand(models.Model):
+    """Модель марок (брендов) машин"""
+    name = models.CharField(max_length=32,
+                            blank=False, null=False,
+                            unique=True,
+                            verbose_name="Марка машины")
+
+    def __str__(self):
+        return f'{self.id} - {self.name}'
+
+
+class Car(models.Model):
+    """Модель машины - название, марка (бренд), цвет"""
+    name = models.CharField(max_length=64,
+                            blank=False, null=False,
+                            verbose_name="Название машины")
+    brand = models.ForeignKey("Brand",
+                              blank=False, null=False,
+                              on_delete=models.PROTECT,
+                              related_name="cars",
+                              verbose_name="Марка машины")
+    color = models.ForeignKey("Color",
+                              blank=False, null=False,
+                              on_delete=models.PROTECT,
+                              related_name="cars",
+                              verbose_name="Цвет")
+
+    def __str__(self):
+        return f"{self.id} - {self.name}, Марка {self.brand}, Цвет {self.color}"
